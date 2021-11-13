d122c22791f3d5215a9e988f2b5753ad0d39c60e
shakederin <89574610+shakederin@users.noreply.github.com>
2021-11-13 13:07:23 +0200

    to heroku 5

--- Backend/app.js
+++ Backend/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use(express.static('../dist/index.html'))
+app.use("/", express.static('../dist/index.html'))
 
 app.get("/cities", (req ,res)=>{
     Agent.find({}).distinct("city").then((cities)=>{
